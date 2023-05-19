a80bc11f1af76c3b6a0ad75e88ba5b18fd17c3fe
ziyangfu <fu438484102@163.com>
2023-05-19 16:50:43 +0800

    add source file : Buffer

--- src/net/Buffer.cpp
+++ src/net/Buffer.cpp
@@ -3,3 +3,38 @@
 //
 
 #include "Buffer.h"
+
+#include "SocketsOps.h"
+#include <errno.h>
+#include <sys/uio.h>
+
+using namespace netflow::net;
+
+const size_t Buffer::kCheapPrepend;
+const size_t Buffer::kInitialSize;
+const char Buffer::kCRLF[] = "\r\n"
+
+ssize_t Buffer::readFd(int fd, int *savedErrno) {
+    char extrabuf[65536];
+    struct iovec vec[2];
+    const size_t writable = writableBytes();
+    vec[0].iov_base = begin() + writerIndex_;
+    vec[0].iov_len = writable;
+    vec[1].iov_base = extrabuf;
+    vec[1].iov_len = sizeof extrabuf;
+
+    const int iovcnt = (writable < sizeof extrabuf) ? 2 : 1;
+    const ssize_t n = sockets::readv(fd,vec, iovcnt);
+    if (n < 0){
+        *savedErrno = errno;
+    }
+    else if (static_cast<size_t>(n) <= writable) {
+        writerIndex_ += n;
+    }
+    else {
+        writerIndex_ = buffer_.size();
+        append(extrabuf, n - writable);
+    }
+    return 0;
+
+}
--- src/net/Buffer.h
+++ src/net/Buffer.h
@@ -12,6 +12,7 @@
 #include <string.h>
 
 
+
 namespace netflow::net {
 /** 非阻塞IO必备， 包括读buffer和写buffer */
 /// A buffer class modeled after org.jboss.netty.buffer.ChannelBuffer
@@ -27,6 +28,112 @@ namespace netflow::net {
 
 class Buffer {
 public:
+    static const size_t kCheapPrepend = 8;
+    static const size_t kInitialSize = 1024;
+
+    Buffer(size_t initialSize = kInitialSize)
+        : buffer_(kCheapPrepend + initialSize),
+          readerIndex_(kCheapPrepend),
+          writerIndex_(kCheapPrepend)
+    {
+        //
+    }
+    ~Buffer();
+
+    void swap(Buffer& rhs){}
+
+    size_t readableBytes() const {}
+    size_t writableBytes() const {}
+    size_t prependableBytes() const {}
+
+    const char* peek() const {}
+
+    const char* findCRLF(){}
+    const char* findCRLF(const char* start){}
+
+    const char* findEOL(){}
+    const char* findEOL(const char* start){}
+
+    /** 取回 */
+    void retrieve(size_t len) {}
+    void retrieveUntil(const char* end){}
+
+    void retrieveInt64(){}
+    void retrieveInt32(){}
+    void retrieveInt16(){}
+    void retrieveInt8(){}
+
+    void retrieveAll(){}
+
+    std::string retrieveAllAsString(){}
+    std::string retrieveAsString(){}
+
+    void append(const char* data, size_t len){}
+    void append(const void* data, size_t len){}
+
+    void ensureWritableBytes(size_t len){}
+
+    char* beginWrite(){}
+    const char* beginWrite() const {}
+    void hasWritten(size_t len){}
+    void unwrite(size_t len){}
+
+    void appendInt64(int64_t x){}
+    void appendInt32(int32_t x){}
+    void appendInt16(int16_t x){}
+    void appendInt8(int8_t x){}
+
+
+    int64_t readInt64(){}
+    int32_t readInt32(){}
+    int16_t readInt16(){}
+    int8_t readInt8(){}
+
+    int64_t peekInt64() const {}
+    int32_t peekInt32() const {}
+    int16_t peekInt16() const {}
+    int8_t peekInt8() const {}
+
+    void prependInt64(int64_t x){}
+    void prependInt32(int32_t x){}
+    void prependInt16(int16_t x){}
+    void prependInt8(int8_t x){}
+
+    void prepend(const void* data, size_t len) {}
+
+    void shrink(size_t reserve){}
+
+    size_t getInternalCapacity() const {}
+
+    ssize_t readFd(int fd, int* savedErrno);  /** 在 Buffer.cpp中实现 */
+private:
+    char* begin()
+    { return &*buffer_.begin(); }
+
+    const char* begin() const
+    { return &*buffer_.begin(); }
+
+    void makeSpace(size_t len)
+    {
+        if (writableBytes() + prependableBytes() < len + kCheapPrepend)
+        {
+            // FIXME: move readable data
+            buffer_.resize(writerIndex_+len);
+        }
+        else
+        {
+            // move readable data to the front, make space inside buffer
+            assert(kCheapPrepend < readerIndex_);
+            size_t readable = readableBytes();
+            std::copy(begin()+readerIndex_,
+                      begin()+writerIndex_,
+                      begin()+kCheapPrepend);
+            readerIndex_ = kCheapPrepend;
+            writerIndex_ = readerIndex_ + readable;
+            assert(readable == readableBytes());
+        }
+    }
+
 private:
     std::vector<char> buffer_;
     size_t readerIndex_;
